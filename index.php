30785f601ffa6ca845de45e9ff1ef292f38cd70c
skutum <relyttech@gmail.com>
2014-04-12 00:11:46 -0400

    Redesigned Site for 2014

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Party Pooper Waste Management</title>
+
+<meta name = "keywords" content = "Portable Restrooms, Porta Pots, Porta Potties, Septic Cleaning, Septic Pumping, Sewer System, Southern Utah, Kanab Utah" />
+<meta name = "description" content = "Party Pooper Specializes in Waste Managment we pump sewers and provide portable mobile restrooms." />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, minimum-scale=1 user-scalable=no">
+
+
+
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,700' rel='stylesheet' type='text/css'>
+
+<link href="css/master.css" rel="stylesheet" type="text/css">
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+<script type="text/javascript" 	src="js/jquery.smint.js"></script>
+
+</head>
+<body onload="setTimeout(function() { window.scrollTo(0, 1) }, 100);">
+
+	<div class="wrap">
+
+	  <div class="subMenu" >
+	 	<div class="inner">
+	 		<a href="#" id="sTop" class="subNavBtn">Home</a> 
+			<a href="#" id="s1" class="subNavBtn">Services</a>
+			<a href="#" id="s4" class="subNavBtn">Contact</a>
+		</div>			
+	</div>
+
+	<div class="section sTop">
+		
+		<div class="inner">
+			<h1>Welcome to Party Pooper! </h1>
+			<p>Your Waste Management Specialists!</p>
+			<img src="img/mainBanner/banner1.jpg">
+		</div>
+		<br class="clear">
+	</div>
+
+	<div class="section s1">
+		
+		<div class="inner">
+
+			<h1>What we can do for you:</h1>
+			<ul>
+			    <li>Clean and Pump Septic, Sewer and Sewage Tanks</li>
+			    <li>Rent Clean Portable restrooms, ADA versions are available and all restrooms include hand sanitizer units.</li>
+			    <li>Rent Portable Handwash Stations</li>
+			    <li>Rent Mobile Trailer Restrooms</li>
+			    <li>Pump and Clean Grease Traps</li>
+			</ul>
+			<br>
+			
+			<br>
+			<p>PARTY POOPER Waste Management is your Southern Utah specialist in septic system pumping, maintenance as well as new construction of septic systems.
+			We also offer rentals of portable restrooms, handwash stations, ADA Wheelchair Restrooms and much more.</p>
+			<br>
+			<img src="img/mainBanner/banner2.jpg"><br><br>
+			<h2>Service Area:</h2>
+			<ul>
+			    <li>Kanab, UT</li>
+			    <li>Orderville, UT</li>
+			    <li>Page, AZ</li>
+			    <li>Big Water, UT</li>
+			    <li>Bryce Canyon, UT</li>
+			    <li>Panguitch, UT</li>
+			    <li>Escalante, UT</li>
+			    <li>Fredonia, AZ</li>
+			    <li>And Many More... Call Us to see if we can service your area!</li>
+			</ul>
+		</div>
+
+	</div>
+
+	<div class="section s4">
+		<div class="inner">
+
+			<h1 style="text-decoration: underline;">Contact Us</h1>
+			<h2>Phone: 435-644-5400</h2>
+			<h2>Email: <a href="mailto:partypooperwm@gmail.com">partypooperwm@gmail.com</a></h2>
+			<h2>170 N. Crescent Butte Trail, Kanab UT, 84741</h2>
+			<img src="img/mainBanner/banner3.jpg"><br><br>
+			
+			<span>Site Developemnt by: <a href="http://skutum.com" target="_blank">skutum.com</a> | Copyright &copy 2014</span>
+		</div>
+
+	</div>
+
+</div>
+<script type="text/javascript">
+	
+
+$(document).ready( function() {
+    $('.subMenu').smint({
+    	'scrollSpeed' : 1000
+    });
+});
+
+
+</script>
+</body>
+</html>
